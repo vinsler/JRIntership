cf9b68de31b9a7815b288f050a2fa7f9db29d79f
Alexei Kurbatov <vinsler@yandex.ru>
2018-06-07 22:04:29 +0300

    add new file loadRecordsToDb.sql

---
+++ src/main/resources/loadRecordsToDb.sql
@@ -0,0 +1,62 @@
+insert into notes (name, description, createDate, status)
+values ('Английский','Почитать про обучение английской грамматике.','2018-04-03', 0);
+
+insert into notes (name, description, createDate, status)
+values ('Комп','Переустановить винду.','2018-04-17', 0);
+
+insert into notes (name, description, createDate, status)
+values ('Сосед бур','Вернуть бур соседу, сказать спасибо.','2018-05-25', 0);
+
+insert into notes (name, description, createDate, status)
+values ('Почистить комп','Пропылесосить комп, почистить клинером.','2018-04-14', 0);
+
+INSERT INTO notes (name, description, createDate, status)
+VALUES ('week beer', 'Go with friends to pub.', '2018-04-02', 0);
+
+insert into notes (name, description, createDate, status)
+values ('Магазин', 'Сходить в магазин купить памперсы', '2018-05-12', 0);
+
+insert into notes (name, description, createDate, status)
+values ('Работа Вт.', '3 заявки по 2 категории, списать инструмент.', '2018-04-05', 0);
+
+insert into notes (name, description, createDate, status)
+values ('На дачу', 'Взять шурик тестю.', '2018-06-01', 1);
+
+insert into notes (name, description, createDate, status)
+values ('Github', 'Залить коммиты на гитхаб.', '2018-03-19', 1);
+
+insert into notes (name, description, createDate, status)
+values ('AliExpress', 'Промониторить камеры на алеэкспрессе.', '2018-04-05', 1);
+
+insert into notes (name, description, createDate, status)
+values ('МАМА', 'Позвонить маме, узнать как у нее дела.', '2018-01-19', 0);
+
+insert into notes (name, description, status)
+values ('Работа Пн.', 'К 7:30 подьехать к ЛЕНТЕ.', 1);
+
+insert into notes (name, description,status)
+values ('Вечером пн.', 'Решить задачи на Javarush.', 0);
+
+insert into notes (name, description, status)
+values ('Вечером вт.', 'Пробурить стену до 20:00', 1);
+
+insert into notes (name, description)
+values ('ХЗ', 'Подключить видеокамеры.');
+
+insert into notes (name, description)
+values ('Сегодня', 'Посмотреть курсы HTML Academy.');
+
+insert into notes (name, description)
+values ('Детям','Посмотреть книжки и наклейки в магазине.');
+
+insert into notes (name, description)
+values ('Пиво', 'Купить пшеничного пива в Градусах, кстати недорого. :-)))');
+
+insert into notes (name, description)
+values ('JDBC SQL', 'Посмотреть видео разных реализаций по технологии.');
+
+insert into notes (name, description)
+values ('SQL-EX', 'Почитать несколько уроков по учебнику, выполнить пару задач.');
+
+insert into notes (name, description)
+values ('Гараж', 'Сьездить в гараж, забрать ЦПС, отвезти тестю.');
