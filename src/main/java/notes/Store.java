4dd982598cf909a2946d5606ac5aa3d33055ccec
Alexei Kurbatov <vinsler@yandex.ru>
2018-06-08 21:26:35 +0300

    reworking method add static init

--- src/main/java/notes/Store.java
+++ src/main/java/notes/Store.java
@@ -6,59 +6,45 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class Store {
-    private static final String URL = "jdbc:mysql://localhost:3306/test";
-    private static final String LOGIN = "root";
-    private static final String PASSWORD = "root";
     private static final String PROPERTIES = "src/main/resources/query.properties";
+    private final Connector connector = Connector.getInstance();
+    private final Properties prop = new Properties();
 
-    public static void main(String[] args) {
-    }
-
-    private Properties NewPropertiesTryLoadGetAction(String action){
+    {
         try {
-            Properties prop = new Properties();
             prop.load(new FileInputStream(PROPERTIES));
-            if (prop.getProperty(action) == null) {
-                throw new Exception();
-            }
-            return prop;
         } catch (IOException e) {
-            System.err.println("Can't find file PROPETIES!");
-        } catch (Exception e) {
-            System.err.println("Can't find syntax - " + action);
+            throw new RuntimeException("Error, can't find file PROPERTIES!", e);
         }
-        return null;
     }
 
     public void insertNote(Note note, int id) throws SQLException {
-        Connection connection = new dbConnect().getConnection(URL, LOGIN, PASSWORD);
-        Properties prop = NewPropertiesTryLoadGetAction("add");
-        java.sql.Date sqlDate = new java.sql.Date(note.getCreateDate().getTime());
-        PreparedStatement preparedStatement = connection.prepareStatement(prop.getProperty("add"));
+        Properties prop = getQuery("add");
+        try (Connection connection = connector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(prop.getProperty("add"))) {
+            java.sql.Date sqlDate = new java.sql.Date(note.getCreateDate().getTime());
 
-        preparedStatement.setInt(1, note.getId());
-        preparedStatement.setString(2, note.getName());
-        preparedStatement.setString(3, note.getDescription());
-        preparedStatement.setDate(4, sqlDate);
-        preparedStatement.setInt(5, note.getStatus());
-        preparedStatement.executeUpdate();
-
-        preparedStatement.close();
-        connection.close();
+            preparedStatement.setInt(1, note.getId());
+            preparedStatement.setString(2, note.getName());
+            preparedStatement.setString(3, note.getDescription());
+            preparedStatement.setDate(4, sqlDate);
+            preparedStatement.setInt(5, note.getStatus());
+            preparedStatement.executeUpdate();
+        }
     }
 
     public void updateNote(Note note, int id) throws SQLException {
-        Connection connection = new dbConnect().getConnection(URL, LOGIN, PASSWORD);
-        Properties prop = NewPropertiesTryLoadGetAction("update");
+        Connection connection = connector.getConnection();
+        Properties prop = getQuery("update");
         java.sql.Date sqlDate = new java.sql.Date(note.getCreateDate().getTime());
         PreparedStatement preparedStatement = connection.prepareStatement(prop.getProperty("update"));
 
         preparedStatement.setString(2, note.getName());
         preparedStatement.setString(3, note.getDescription());
-        preparedStatement.setDate(4, sqlDate);
         preparedStatement.setInt(5, note.getStatus());
         preparedStatement.executeUpdate();
 
@@ -67,8 +53,8 @@ public class Store {
     }
 
     public void deleteNote(Note note, int id) throws SQLException {
-        Connection connection = new dbConnect().getConnection(URL, LOGIN, PASSWORD);
-        Properties prop = NewPropertiesTryLoadGetAction("delete");
+        Connection connection = connector.getConnection();
+        Properties prop = getQuery("delete");
         PreparedStatement preparedStatement = connection.prepareStatement(prop.getProperty("delete"));
 
         preparedStatement.setInt(1, note.getId());
@@ -78,43 +64,59 @@ public class Store {
         connection.close();
     }
 
-    public void soutResult(ResultSet resultSet) throws SQLException {
-        String.format("id : %d \t name : %s \n description : %s \n createDate : %s \t status : %s",
-                resultSet.getInt(1),
-                resultSet.getString(2),
-                resultSet.getString(3),
-                resultSet.getString(4),
-                resultSet.getString(5));
-    }
-
-    public void findNote(Note note, int id) throws SQLException {
-        Connection connection = new dbConnect().getConnection(URL, LOGIN, PASSWORD);
-        Properties prop = NewPropertiesTryLoadGetAction("findById");
-        PreparedStatement preparedStatement = connection.prepareStatement(prop.getProperty("findById"));
-
-        preparedStatement.setInt(1, note.getId());
-        ResultSet result = preparedStatement.executeQuery();
-        if (result != null) {
-            soutResult(result);
+    public Note findNote(Note note, int id) throws SQLException { // return NULL || new Note
+        Properties prop = getQuery("findById");
+        try (Connection connection = connector.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(prop.getProperty("findById"))){
+            preparedStatement.setInt(1, note.getId());
+            ResultSet result = preparedStatement.executeQuery();
+            if (result.next()) {
+                return saveToNote(result, note);
+            } else {
+                return null;
+            }
         }
-        preparedStatement.close();
-        connection.close();
     }
 
-    public void findAllNote(Note note) throws SQLException {
-        Connection connection = new dbConnect().getConnection(URL, LOGIN, PASSWORD);
-        Properties prop = NewPropertiesTryLoadGetAction("findAll");
-        PreparedStatement preparedStatement = connection.prepareStatement(prop.getProperty("findAll"));
-
-        ResultSet result = preparedStatement.executeQuery();
-        while (result.next()) {
-            soutResult(result);
+    public ArrayList<Note> findAllNote(Note note) throws SQLException { // return Arraylist || NULL
+        ArrayList<Note> arrListNote = new ArrayList<Note>();
+        Properties prop = getQuery("findAll");
+        try (Connection connection = connector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(prop.getProperty("findAll"))){
+            ResultSet result = preparedStatement.executeQuery();
+            int num = 0;
+            while (result.next()) {
+                saveToNote(result, arrListNote.get(num));
+                num++;
+            }
+            return arrListNote;
         }
-
-        preparedStatement.close();
-        connection.close();
     }
 
+    private Note saveToNote(ResultSet resultSet, Note note) throws SQLException {
+        while (resultSet.next()) {
+            note.setId(resultSet.getInt(1));
+            note.setName(resultSet.getString(2));
+            note.setDescription(resultSet.getString(3));
+            note.setCreateDate(resultSet.getDate(4));
+            note.setStatus(resultSet.getInt(5));
+        }
+        return note;
+    }
 
-
+    private Properties getQuery(String query) {
+        if (prop.getProperty(query) == null) {
+            throw new RuntimeException("Error, can't find syntax - " + query);
+        }
+        return prop;
+    }
 }
+
+// todo for view test
+//        String.format("id : %d \t name : %s \n description : %s \n createDate : %s \t status : %s",
+//                resultSet.getInt(1),
+//                resultSet.getString(2),
+//                resultSet.getString(3),
+//                resultSet.getString(4),
+//                resultSet.getString(5));
+//    }
