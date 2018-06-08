6d07f54f660c7c6e0bc363c41d8dd3b60aaedce0
Alexei Kurbatov <vinsler@yandex.ru>
2018-06-08 21:24:08 +0300

    refaktor getinstance

---
+++ src/main/java/notes/Connector.java
@@ -0,0 +1,33 @@
+package notes;
+
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connector {
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "root";
+
+    private Connector() {}
+
+    public static Connector getInstance() {
+        try {
+            DriverManager.registerDriver(new FabricMySQLDriver());
+            return new Connector();
+        } catch (SQLException e) {
+            System.err.println("Error connect driver!");
+            throw new RuntimeException();
+        }
+    }
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, LOGIN, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Error connect to datebase!");
+            throw new RuntimeException();
+        }
+    }
+}
