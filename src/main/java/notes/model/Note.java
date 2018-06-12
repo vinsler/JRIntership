50e04312c451e7ce1b68f43d7c39f4391d8a44b2
Alexei Kurbatov <vinsler@yandex.ru>
2018-06-12 23:01:01 +0300

    modifed style

--- src/main/java/notes/model/Note.java
+++ src/main/java/notes/model/Note.java
@@ -1,4 +1,4 @@
-package notes;
+package notes.model;
 
 import java.util.Date;
 
@@ -9,44 +9,37 @@ public class Note {
     private int status;
     private Date createDate;
 
-    public int getStatus() {
-        return status;
-    }
-
-    public void setId(int id) {
+    public void setNote(int id) {
         this.id = id;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
     }
 
     public int getId() {
         return id;
     }
-
     public String getName() {
         return name;
     }
-
     public String getDescription() {
         return description;
     }
-
+    public int getStatus() {
+        return status;
+    }
     public Date getCreateDate() {
         return createDate;
     }
 
-    public void setNote(int id) {
+    public void setId(int id) {
         this.id = id;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public void setDescription(String description) {
         this.description = description;
+    }
+    public void setStatus(int status) {
+        this.status = status;
     }
 
     public void setCreateDate(Date createDate) {
