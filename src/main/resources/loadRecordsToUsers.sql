796aa8a47f21b2e78517a9be057044c794683cc0
Alexei Kurbatov <vinsler@yandex.ru>
2018-06-13 23:59:30 +0300

    some change in *.sql files

---
+++ src/main/resources/loadRecordsToUsers.sql
@@ -0,0 +1,5 @@
+insert into users (name, login, password) values ('Лёха', 'vins', 'root');
+
+insert into users (name, login, password) values ('Мама', 'murzik', 'toot');
+
+insert into users (name, login, password) values ('Папа', 'bill', 'gei');
