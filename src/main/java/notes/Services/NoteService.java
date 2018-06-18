74c5ad2207b6285a0bf3da799d9eee0d1fe9404a
Alexei Kurbatov <vinsler@yandex.ru>
2018-06-18 23:34:58 +0300

    rework NoteService

--- src/main/java/notes/Services/NoteService.java
+++ src/main/java/notes/Services/NoteService.java
@@ -1,53 +1,79 @@
 package notes.Services;
 
 import notes.dao.NoteStore;
+import notes.dao.UsersStore;
+import notes.exception.ServiceException;
 import notes.model.Note;
-import notes.model.Users;
 
 import java.util.ArrayList;
 
 public class NoteService {
-    private static final NoteStore notestore = NoteStore.getInstance();
+    private static final NoteStore NOTESTORE = NoteStore.getInstance();
+    private static final UsersStore USERSTORE = UsersStore.getInstance();
+    private static final String SERVICE_ERR_USER = "ERROR!!! CHECK USER.ID!";
+    private static final String SERVICE_ERR_NAME = "ERROR!!! CHECK USER.NAME!";
+    private static final String SERVICE_ERR_UPDATE = "ERROR!!! CHECK INPUT FOR UPDATE!";
+    private static final String SERVICE_ERR_DELETE = "ERROR!!! CHECK ID FOR DELETE!";
+
 
     public void add(Note note) {
-        Users users = new Users();
         if (note.getName() == null) {
-            System.err.println("ERROR! Check Name!");
-        } else if (note.getDescription() == null) {
-            System.err.println("ERROR! Check Description!");
-        } else if (note.getUsers_id() < 1) {
-            System.out.println("ERROR! Check Users_id");
+            throw new ServiceException(SERVICE_ERR_NAME);
+        } else if (note.getUser().getId() < 1) {
+            throw  new ServiceException(SERVICE_ERR_USER);
         } else {
-            notestore.add(note);
+            try {
+                NOTESTORE.add(note);
+            } catch (RuntimeException e) {
+                throw new ServiceException(SERVICE_ERR_USER, e);
+            }
         }
     }
 
     public void update(Note note, Integer i) {
-        // check
-        notestore.update(note, i);
-        // check
+        if (note.getName() == null && note.getUser().getId() == 0) {
+            throw new ServiceException(SERVICE_ERR_UPDATE);
+        } else if (i < 1) {
+            throw new ServiceException(SERVICE_ERR_USER);
+        } else {
+            try {
+                NOTESTORE.update(note, i);
+            } catch (RuntimeException e) {
+                throw new ServiceException(SERVICE_ERR_USER, e);
+            }
+        }
     }
 
     public void delete(Integer i) {
-        // check
-        notestore.delete(i);
-        // check
+        try {
+            NOTESTORE.delete(i);
+        } catch (RuntimeException e) {
+            throw new ServiceException(SERVICE_ERR_DELETE, e);
+        }
     }
 
     public Note findOne(Integer i) {
-        Note note = new Note();
-        // check
-        note = notestore.findOne(i);
-        // check
-        // transfer to view
-        return note;
+        // todo check exist table
+        Note note = NOTESTORE.findOne(i);
+        if (note != null) {
+            System.out.println(note.getId() + " - " + note.getName() + " - " + note.getDescription());
+            return note;
+        }
+
+        System.out.println("Sry, you note does't find.");
+        return null;
     }
 
     public ArrayList<Note> findAll() {
         ArrayList<Note> listNote = new ArrayList<>();
-        // check
-        listNote = notestore.findAll();
-        // check
+        // todo check exist table
+        listNote = NOTESTORE.findAll();
+
+        if (listNote.isEmpty()) {
+            System.out.println("Database is empty.");
+        }
+
         return listNote;
     }
 }
+
