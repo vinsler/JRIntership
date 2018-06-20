89acb4148d85474be6257e709fe67a0d66bf278f
Alexei Kurbatov <vinsler@yandex.ru>
2018-06-20 22:57:51 +0300

    Rework resources

--- src/main/resources/createDbTest.sql
+++ src/main/resources/createDbTest.sql
@@ -1,16 +1,16 @@
-CREATE DATABASE IF NOT EXISTS test DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+create database if not exists test default character set utf8 collate utf8_general_ci;
 
-USE test;
+use test;
 
-CREATE TABLE IF NOT EXISTS notes (
+create table if not exists notes (
 
-  id            int(11) AUTO_INCREMENT,
+  id            int(11) auto_increment,
   name          varchar(20) not null,
   description   varchar(100),
-  create_date   datetime NOT NULL,
-  status        tinyint(1) NOT NULL DEFAULT 0,
+  create_date   datetime not null,
+  status        tinyint(1) not null default 0,
   users_id      int not null,
 
   primary key (id),
-  FOREIGN KEY (users_id) REFERENCES users(id)
+  foreign key (users_id) references users(id)
 ) default character set utf8;
--- src/main/resources/createDbUser.sql
+++ src/main/resources/createDbUser.sql
@@ -4,10 +4,10 @@ use test;
 
 create table if not exists users (
 
-  id          int(11) auto_increment,
-  name        varchar(20) not null,
-  login       varchar(20) not null unique,
-  password    varchar(20) not null,
+  id            int(11) auto_increment,
+  name          varchar(20) not null,
+  login         varchar(20) not null unique,
+  password      varchar(20) not null,
 
   primary key (id)
 ) default character set utf8;
