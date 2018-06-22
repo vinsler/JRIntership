c3f06096521ba5b8c712f18441f3439a18d59db6
Alexei Kurbatov <vinsler@yandex.ru>
2018-06-22 19:12:25 +0300

    Some changes in exeption

--- src/main/java/notes/exception/ResourceException.java
+++ src/main/java/notes/exception/ResourceException.java
@@ -3,8 +3,9 @@ package notes.exception;
 public class ResourceException extends RuntimeException {
     private static final String ERR_NOT_FOUND = "Service error, resources not found!";
 
-    public ResourceException(String message) {
-        super(message);
+    public ResourceException(Exception e) {
+        super(e);
+        System.out.println(ERR_NOT_FOUND);
     }
 
     public ResourceException(){
