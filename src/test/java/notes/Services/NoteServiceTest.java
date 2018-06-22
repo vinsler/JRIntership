a0b70f49d5b54a41650f8f663d5a910db8c331d8
Alexei Kurbatov <vinsler@yandex.ru>
2018-06-22 19:14:10 +0300

    add maven test dependecies

---
+++ src/test/java/notes/Services/NoteServiceTest.java
@@ -0,0 +1,38 @@
+package notes.Services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NoteServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void findOne() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+}
