f42dd918af7fa26b25e7ebba6b6eb510c3a52b20
Alexei Kurbatov <vinsler@yandex.ru>
2018-06-22 19:11:24 +0300

    Some changes in pckg Services

--- src/main/java/notes/Services/NoteService.java
+++ src/main/java/notes/Services/NoteService.java
@@ -2,79 +2,50 @@ package notes.Services;
 
 import notes.dao.NoteStore;
 import notes.dao.Store;
-import notes.exception.ResourceException;
 import notes.exception.ValidationException;
-import notes.exception.SqlAccessException;
 import notes.model.Note;
 
-import java.util.ArrayList;
 import java.util.List;
-
-// todo check exist table BEFORE use NOTESTORE !!!
 
 public class NoteService {
     private static final Store NOTESTORE = NoteStore.getInstance();
 
     public void add(Note note) {
         checkNote(note, 1);
-        try {
-            NOTESTORE.add(note);
-        } catch (SqlAccessException e) {
-                // вот сюда выбрасывается SqlAccessException(SQL_ERR_MSG, e);
-            e.printStackTrace();
-        }
+        NOTESTORE.add(note);
     }
 
     public void update(Note note, Integer i) {
         checkNote(note, i);
-        try {
-            NOTESTORE.update(note, i);
-        } catch (SqlAccessException e) {
-            e.printStackTrace();
-        }
+        NOTESTORE.update(note, i);
     }
 
     public void delete(Integer i) {
-        if (NOTESTORE.findOne(i) != null) {
-            try {
-                NOTESTORE.delete(i);
-            } catch (SqlAccessException e) {
-                e.printStackTrace();
-            }
-        } else {
-            throw new ResourceException();
+        if (i == null || i <= 0) {
+            throw new ValidationException("ERR_ID");
         }
+        NOTESTORE.delete(i);
+
     }
 
     public Note findOne(Integer i) {
-        try {
-            Note note = (Note) NOTESTORE.findOne(i);
-            if (note != null) {
-                return note;
-            }
-            return null;
-        } catch (SqlAccessException e) {
-            e.printStackTrace();
-            throw new ResourceException();
+        if (i == null || i <= 0) {
+            throw new ValidationException("ERR_ID");
         }
+        return (Note) NOTESTORE.findOne(i);
+
     }
 
     public List<Note> findAll() {
-        List<Note> listNote = new ArrayList<>();
-        try {
-            listNote = NOTESTORE.findAll();
-            if (!listNote.isEmpty()) {
-                return listNote;
-            }
-            return listNote;
-        } catch (SqlAccessException e) {
-            e.printStackTrace();
-            throw new ResourceException();
-        }
+
+        return NOTESTORE.findAll();
+
     }
 
     private void checkNote(Note note, Integer i) {
-        if (note.getName() == null) {
+        if (i == null || i <= 0) {
+            throw new ValidationException("ERR_ID");
+        } else if (note.getName() == null) {
             throw new ValidationException("ERR_NAME");
         } else if (note.getUser() == null) {
             throw new ValidationException("ERR_USER_NOT_FOUND");
--- src/main/java/notes/Services/UserService.java
+++ src/main/java/notes/Services/UserService.java
@@ -1,88 +1,52 @@
 package notes.Services;
 
+import notes.dao.Store;
 import notes.dao.UsersStore;
-import notes.exception.ResourceException;
 import notes.exception.ValidationException;
-import notes.exception.SqlAccessException;
-import notes.model.Users;
+import notes.model.User;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
-// todo check exist table BEFORE use userstore !!!
-
 public class UserService {
-    private static final UsersStore USERSTORE = UsersStore.getInstance();
-//    private static final String ERR_ID = "Service, check id!";
-//    private static final String ERR_NAME = "Service, check name!";
-//    private static final String ERR_LOGIN = "Service, check login!";
-//    private static final String ERR_PASSWORD = "Service, check password!";
-//    private static final String ERR_UPDATE_DELETE = "Service, check that the data for the actions is correct!";
-//    private static final String ERR_NOT_FOUND = "Service, resources not found!";
+    private static final Store USERSTORE = UsersStore.getInstance();
 
-    public void add(Users users){
-        if (users.getId() == 0) {
-            throw new ValidationException("ERR_ID");
-        } else if (users.getName() == null) {
+    public void add(User user){
+        if (user.getName() == null) {
             throw new ValidationException("ERR_NAME");
-        }  else if (users.getLogin() == null) {
+        }  else if (user.getLogin() == null) {
             throw new ValidationException("ERR_LOGIN");
-        } else if (users.getPassword() == null) {
+        } else if (user.getPassword() == null) {
             throw new ValidationException("ERR_PASSWORD");
         }
-        try {
-            USERSTORE.add(users);
-        } catch (SqlAccessException e) {
-            e.printStackTrace();
-        }
+        USERSTORE.add(user);
     }
 
-    public void update (Users users, Integer i){
-        if (i == 0) {
+    public void update (User user, Integer i){
+        if (user == null) {
+            throw new ValidationException("ERR_USER_NOT_FOUND");
+        } else if (i == null || i <= 0) {
             throw new ValidationException("ERR_UPDATE");
         }
-        try {
-            USERSTORE.update(users, i);
-        } catch (SqlAccessException e) {
-            e.printStackTrace();
-        }
+        USERSTORE.update(user, i);
     }
 
     public void delete(Integer i){
-        if (i == 0) {
+        if (i == null || i <= 0) {
             throw new ValidationException("ERR_DELETE");
         }
-        try {
-            USERSTORE.delete(i);
-        } catch (SqlAccessException e) {
-            e.printStackTrace();
-        }
+        USERSTORE.delete(i);
     }
 
-    public Users findOne(Integer i){
-        try {
-            Users users = USERSTORE.findOne(i);
-            if (users != null) {
-                return users;
-            }
-            return null;
-        } catch (SqlAccessException e) {
-            e.printStackTrace();
-            throw new ResourceException("ERR_NOT_FOUND");
+    public User findOne(Integer i){
+        if (i == null || i <= 0) {
+            throw new ValidationException("ERR_ID");
         }
+        return (User) USERSTORE.findOne(i);
     }
 
-    public List<Users> findAll(){
-        List<Users> listUsers = new ArrayList<>();
-        try {
-            listUsers = USERSTORE.findAll();
-            if (!listUsers.isEmpty()) {
-                return listUsers;
-            }
-            return listUsers;
-        } catch (SqlAccessException e) {
-            e.printStackTrace();
-            throw new ResourceException("ERR_NOT_FOUND");
-        }
+    public List<User> findAll(){
+        return USERSTORE.findAll();
+
     }
 }
