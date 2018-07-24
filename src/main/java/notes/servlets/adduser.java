a9d4e6f21f6d0b798fcc0e8fdadbbc8f1bf46e49
Alexei Kurbatov <vinsler@yandex.ru>
2018-07-24 23:16:12 +0300

    add 2 servlets

--- src/main/java/notes/servlets/adduser.java
+++ src/main/java/notes/servlets/adduser.java
@@ -1,5 +1,9 @@
 package notes.servlets;
 
+import notes.Services.UserService;
+import notes.model.User;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,17 +13,32 @@ import java.io.PrintWriter;
 
 public class adduser extends HttpServlet {
 
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doGet(req, resp);
-        PrintWriter pw = resp.getWriter();
-        pw.println("<HTML>");
-        pw.println("<h1 align=\"center\"> please enter you name </h1>");
-        pw.println("</HTML>");
-        pw.close();
+//        PrintWriter writer = resp.getWriter();
+//        writer.println("method doGet from adduser.");
+
+
     }
 
-
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String name = req.getParameter("name");
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        PrintWriter pw = resp.getWriter();
+        pw.println("name-" + name + ", login- " + login + ", password- " + password);
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("view/viewnote.jsp");
+        requestDispatcher.forward(req, resp);
+//
+//        UserService userService = new UserService();
+//        User user = new User();
+//        user.setName(name);
+//        user.setLogin(login);
+//        user.setPassword(password);
+//        userService.add(user);
     }
 }
