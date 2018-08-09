7e0a2e381ada1da11a6a152aef2e49b0e335c2b5
Alexei Kurbatov <vinsler@yandex.ru>
2018-08-09 22:40:14 +0300

    add loginuser & start view note

--- src/main/java/notes/servlets/loginuserServlet.java
+++ src/main/java/notes/servlets/loginuserServlet.java
@@ -1,12 +1,15 @@
 package notes.servlets;
 
 import notes.Services.UserService;
+import notes.exception.ValidationException;
 import notes.model.User;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class loginuserServlet extends HttpServlet {
@@ -17,11 +20,18 @@ public class loginuserServlet extends HttpServlet {
         User user = new User();
         user.setLogin(req.getParameter("login"));
         user.setPassword(req.getParameter("password"));
-
-
-
-        //USER_SERVICE.findOne(req.getParameter(""))
-
+        try {
+            USER_SERVICE.findLogin(user);
+        } catch (ValidationException e) {
+            req.setAttribute("message", "pls check you data");
+            RequestDispatcher requestDispatcher = req.getRequestDispatcher("/view/loginuser.jsp");
+            requestDispatcher.forward(req, resp);
+            return;
+        }
+        HttpSession session = req.getSession();
+        session.setAttribute("login", user.getLogin());
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/viewnote");
+        requestDispatcher.forward(req, resp);
     }
 
 }
--- src/main/java/notes/servlets/viewnoteServlet.java
+++ src/main/java/notes/servlets/viewnoteServlet.java
@@ -1,15 +1,27 @@
 package notes.servlets;
 
+import notes.Services.NoteService;
+import notes.model.Note;
+import notes.model.User;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class viewnoteServlet extends HttpServlet {
+    private NoteService noteService = new NoteService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // req.setAttribute("Users", );
+
+        List<Note> notelist =  noteService.findAll(); // todo add findbylogin in notes
+
+        req.setAttribute("notelist", notelist);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/view/viewnote.jsp");
+        dispatcher.forward(req, resp);
     }
 }
