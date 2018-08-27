62f2370fc320ee9c47f3f718e89136585aee0f48
Alexei Kurbatov <vinsler@yandex.ru>
2018-08-27 22:39:31 +0300

    change sortnoteServlet

--- src/main/java/notes/servlets/sortnoteServlet.java
+++ src/main/java/notes/servlets/sortnoteServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
@@ -30,26 +31,31 @@ public class sortnoteServlet extends HttpServlet {
         switch (req.getParameter("id")) {
             case "1": {
                 Collections.sort(notelist, new NoteIdComparator());
+                req.getSession().setAttribute("sortid", 1);
                 reqSetRequestDispatcher(req, resp);
                 break;
             }
             case "2": {
                 Collections.sort(notelist, new NoteNameComparator());
+                req.getSession().setAttribute("sortid", 2);
                 reqSetRequestDispatcher(req, resp);
                 break;
             }
             case "3": {
                 Collections.sort(notelist, new NoteDescriptionComparator());
+                req.getSession().setAttribute("sortid", 3);
                 reqSetRequestDispatcher(req, resp);
                 break;
             }
             case "4": {
                 Collections.sort(notelist, new NoteCreateDateComparator());
+                req.getSession().setAttribute("sortid", 4);
                 reqSetRequestDispatcher(req, resp);
                 break;
             }
             case "5": {
                 Collections.sort(notelist, new NoteStatusComparator());
+                req.getSession().setAttribute("sortid", 5);
                 reqSetRequestDispatcher(req, resp);
                 break;
             }
@@ -57,8 +63,43 @@ public class sortnoteServlet extends HttpServlet {
     }
 
     private void reqSetRequestDispatcher(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("sortnote", notelist);
+        HttpSession session = req.getSession();
+        session.setAttribute("sortnote", notelist);
+
+        Integer tmpint = (int)(long) req.getSession().getAttribute("pointer");
+        if (tmpint >= notelist.size()) {
+            req.getSession().setAttribute("pointer", notelist.size() - notelist.size() % 10);
+        } else {
+            req.getSession().setAttribute("pointer", tmpint - 10);
+        }
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("/viewnote");
         requestDispatcher.forward(req, resp);
     }
+
+    public List<Note> sortNoteList(HttpServletRequest req, List<Note> srtnotelist){
+        switch ((int) req.getSession().getAttribute("sortid")) {
+            case 1: {
+                Collections.sort(srtnotelist, new NoteIdComparator());
+                break;
+            }
+            case 2: {
+                Collections.sort(srtnotelist, new NoteNameComparator());
+                break;
+            }
+            case 3: {
+                Collections.sort(srtnotelist, new NoteDescriptionComparator());
+                break;
+            }
+            case 4: {
+                Collections.sort(srtnotelist, new NoteCreateDateComparator());
+                break;
+            }
+            case 5: {
+                Collections.sort(srtnotelist, new NoteStatusComparator());
+                break;
+            }
+        }
+        return srtnotelist;
+    }
 }
+
